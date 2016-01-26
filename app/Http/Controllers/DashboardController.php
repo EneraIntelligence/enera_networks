603e8d66b9886a7ce5b971a3bc352b4a8735fc21
Eder Díaz <ederchrono@gmail.com>
2016-01-26 10:50:47 -0600

    wordcloud testing a producción

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 
 use Networks\Http\Requests;
 use Networks\Http\Controllers\Controller;
+use DB;
+use MongoId;
 
 class DashboardController extends Controller
 {
@@ -17,6 +19,77 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboard.index', ['user' => Auth::user()]);
+
+        $cLogsColl = DB::getMongoDB()->selectCollection('campaign_logs');
+        $users = $cLogsColl->aggregate([
+            [
+                '$group' => [
+                    '_id'=>'none',
+                    'ids'=>['$addToSet'=>'$user.id']
+                ]
+            ]
+        ]);
+
+        $userIdsArray = $users['result'][0]['ids'];
+
+        foreach($userIdsArray as $separateIds){
+            $_ids[] = $separateIds instanceof MongoId ? $separateIds : new MongoId($separateIds);
+        }
+        //dd($_ids);
+
+        $likes = DB::getMongoDB()->selectCollection('users')->aggregate([
+            [
+                '$match'=>[
+                    '_id'=>['$in'=>$_ids]
+                ],
+            ],
+            [
+              '$unwind'=>'$facebook.likes'
+            ],
+            [
+                '$group' => [
+                    '_id'=>'$facebook.likes',
+                    'count'=>['$sum'=>1]
+                ]
+            ],
+            [
+                '$sort'=>['count'=>-1]
+            ],
+            [
+                '$limit'=>30
+            ]
+        ]);
+
+        //dd($likes['result']);
+
+        foreach($likes['result'] as $k=>$v){
+            //dd($v);
+            $likes_ids[] = $v['_id'];
+            $likes_counts[] = $v['count'];
+        }
+
+        $FbColl = DB::getMongoDB()->selectCollection('facebook_pages');
+        $pages_cursor = $FbColl->aggregate([
+            [
+                '$match'=>[
+                    'id'=>['$in'=>$likes_ids]
+                ]
+            ],
+            [
+                '$project'=>[
+                    '_id'=>'$id',
+                    'name'=>1
+                ]
+            ]
+        ]);
+
+        //$pages_cursor = array_merge($pages_cursor['result'],$likes_counts);
+        //dd($pages_cursor);
+        $words = $pages_cursor['result'];
+        $wordCount = $likes['result'];
+
+//        dd($wordCount);
+
+        return view('dashboard.index', ['user' => Auth::user(),'words'=>$words,'wordCount'=>$wordCount]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -21,16 +21,28 @@
         var canvasWidth = 600;
         var canvasHeight = 400;
 
-        var wordValues = {".NET":5, "Silverlight":10, "jQuery":300, "CSS3":20, "HTML5":400, "JavaScript":200, "SQL":15,"C#":20}
+        //var wordValues = {".NET":5, "Silverlight":10, "jQuery":300, "CSS3":20, "HTML5":400, "JavaScript":200, "SQL":15,"C#":20}
+        var wordValues = {
+            @for($i=0;$i<count($words);$i++)
+                "{!! $words[$i]['name'] !!}":{!! $wordCount[$i]['count'] !!},
+            @endfor
+        };
+
+        var words = [
+            @for($i=0;$i<count($words);$i++)
+                "{!! $words[$i]['name'] !!}",
+            @endfor
+        ];
 
         var sumWords = 0;
         for (var k in wordValues){
             sumWords += wordValues[k];
         }
 
+
+
         d3.layout.cloud().size([canvasWidth, canvasHeight])
-                .words([
-                    ".NET", "Silverlight", "jQuery", "CSS3", "HTML5", "JavaScript", "SQL","C#"].map(function(d) {
+                .words(words.map(function(d) {
                     return {text: d, size: 50*(Math.log( wordValues[d] )/Math.log(sumWords)) };
                 }))
                 .rotate(function() { return 0; })
@@ -44,7 +56,7 @@
                     .attr("width", canvasWidth)
                     .attr("height", canvasHeight)
                     .append("g")
-                    .attr("transform", "translate(150,150)")
+                    .attr("transform", "translate("+canvasWidth/2+","+canvasHeight/2+")")
                     .selectAll("text")
                     .data(words)
                     .enter().append("text")
