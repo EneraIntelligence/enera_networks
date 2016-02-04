dc499b56e179e9d9686c5d152511ba79438447ce
Pedro Luna <pluna@enera.mx>
2016-02-04 17:29:46 -0600

    try add devices (distinct)

--- app/Http/Controllers/BranchesController.php
+++ app/Http/Controllers/BranchesController.php
@@ -36,7 +36,7 @@ class BranchesController extends Controller
             return view('branches.show', [
                 'branch' => $branch,
                 'network' => Network::find(session('network_id')),
-                'devices' => $branch->campaign_logs()->distinct('device.mac')->get(),
+                'devices' => $branch->campaign_logs()->distinct('device.mac')->count(),
             ]);
         } else {
             return redirect()->route('branches::index')->with([
