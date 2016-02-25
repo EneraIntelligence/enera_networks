9278165c868a0277b07958708292a0abfbbbb845
Pedro Luna <pluna@enera.mx>
2016-02-25 16:41:09 -0600

    [fix] DashboardController > index

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -103,7 +103,7 @@ class DashboardController extends Controller
             ],
         ]);
 
-        return $devices['result'][0]['count'];
+        return isset($devices['result'][0]['count']) ? $devices['result'][0]['count'] : 0;
 
     }
 
@@ -128,7 +128,7 @@ class DashboardController extends Controller
             ]
         ]);
 
-        return $devices['result'][0]['count'];
+        return isset($devices['result'][0]['count']) ? $devices['result'][0]['count'] : 0;
 
 
     }
