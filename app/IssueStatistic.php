78b1454503898cc073d275b50bc11e0ab22ee12c
Pedro Luna <pluna@enera.mx>
2016-04-26 12:21:27 -0500

    [FIX] IssueTracker

---
+++ app/IssueStatistic.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Networks;
+
+use Jenssegers\Mongodb\Model as Model;
+
+class IssueStatistic extends Model
+{
+    protected $fillable = ['date', 'recurrence', 'host'];
+    protected $dates = ['created_at', 'updated_at'];
+    // relations
+
+    // end relations
+}
