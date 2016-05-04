00f478c2a43b411d2415a647a9b879427d36731c
Alfonso Rodriguez <alfonso@vizinova.com>
2016-05-04 17:34:43 -0500

    new layout view

--- resources/views/branches/show.blade.php
+++ resources/views/branches/show.blade.php
@@ -3,27 +3,18 @@
 @section('title', 'Nodos')
 
 @section('head_scripts')
-
+    {!! HTML::style('assets/css/nodes.css') !!}
 @stop
 
 @section('content')
-    <div class="container show-on-small">
-        <div class="row">
-            <div class="col s12 m6">
-                <div class="card blue-grey darken-1">
-                    <div class="card-content white-text">
-                        <span class="card-title">Card Title</span>
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                            I am convenient because I require little markup to use effectively.</p>
-                    </div>
-                    <div class="card-action">
-                        <a href="#">This is a link</a>
-                        <a href="#">This is a link</a>
-                    </div>
+        <div class="col s12">
+            <div class="card blue-grey white zero">
+                <div class="card-content black-text show">
+                    <span class="card-title">{{ $branch->name }}</span>
+                    <p class="">{{ $branch->network->name }}</p>
                 </div>
             </div>
         </div>
-    </div>
 
 @endsection
 
