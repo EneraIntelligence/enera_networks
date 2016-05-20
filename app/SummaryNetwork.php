fee96805daaaac1d86bd626ad15e86eadb2271e8
Alfonso Rodriguez <alfonso@vizinova.com>
2016-05-20 11:04:08 -0500

    new model SummaryNetwork changes in profile.index view

---
+++ app/SummaryNetwork.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Networks;
+
+//use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Model;
+
+class SummaryNetwork extends Model
+{
+    //
+}
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -11,35 +11,37 @@
 
 @section('content')
 
-    <div class="card profile">
+    <div class="card profile hide-on-med-and-up">
         <div class="card-image">
-
             <div class="profile-gradient">
                 <img class="profile-image" src="https://s3-us-west-1.amazonaws.com/enera-publishers/avatars/{!! isset($user->image) ? $user->image : 'user.png'!!}" alt="User avatar"/>
             </div>
-
             <span class="card-title">{!! $user->name['first'] . " " . $user->name['last'] !!}</span>
         </div>
-
         <div class="card-content">
-
             <!-- edit FAB -->
             <a class="profile-edit z-depth-2 right btn-floating btn-large waves-effect waves-light red"><i class="material-icons">mode_edit</i></a>
-
-
             <h5 class="flow-text truncate">
                 <i class="material-icons prefix">email</i> &nbsp {{$user->email}}
             </h5>
-
             {{--<div class="divider"></div>--}}
-
             <h5 class="flow-text">
                 <i class="material-icons">phone</i> &nbsp {{$user->phones['number']}}
             </h5>
-
-
         </div>
 
+    </div>
+
+    <div class="hide-on-small-only">
+        <div class="container">
+            <div class="row">
+                <div class="col m6">
+                    <div class="card blue-grey darken-1">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 @stop
