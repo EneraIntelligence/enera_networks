fdf725c85e8843ffd74531a83b9e32b3e71072d8
Alfonso Rodriguez <alfonso@vizinova.com>
2016-05-24 11:50:04 -0500

    add some style to campaign.index

--- resources/views/campaign/index.blade.php
+++ resources/views/campaign/index.blade.php
@@ -57,46 +57,48 @@
 
 
     <div class="hide-on-small-only">
-        <div class="row">
-            @foreach($campaigns as $campaign)
-                <div class="col m6 l4">
-                    <div class="card grey darken-3 white-text">
-                        <div class="card-content white-text">
-                            <div class="card-image">
-                                <img src="{{"https://s3-us-west-1.amazonaws.com/enera-publishers/items/". ($campaign->interaction['name'] != 'survey' ? $campaign->content['images']['large'] : $campaign->content['images']['survey'])}}"
-                                class="responsive-img">
-                                <span class="card-title">{{$campaign->name}}</span>
-                            </div>
-                            {{--<span class="card-title black-text">{{$campaign->name}}</span>--}}
-                            <ul class="list white-text">
-                                <li data-icon="keyboard_arrow_right">Status: {{$campaign->status}}</li>
-                                <hr>
-                                <li data-icon="keyboard_arrow_right">Administrador: {{$campaign->administrator->name['first'].
+        <div class="container">
+            <div class="row">
+                @foreach($campaigns as $campaign)
+                    <div class="col m6 l4">
+                        <div class="card grey darken-3 white-text">
+                            <div class="card-content white-text">
+                                <div class="card-image">
+                                    <img src="{{"https://s3-us-west-1.amazonaws.com/enera-publishers/items/". ($campaign->interaction['name'] != 'survey' ? $campaign->content['images']['large'] : $campaign->content['images']['survey'])}}"
+                                         class="responsive-img">
+                                    <span class="card-title">{{$campaign->name}}</span>
+                                </div>
+                                {{--<span class="card-title black-text">{{$campaign->name}}</span>--}}
+                                <ul class="list white-text">
+                                    <li data-icon="keyboard_arrow_right">Status: {{$campaign->status}}</li>
+                                    <hr>
+                                    <li data-icon="keyboard_arrow_right">Administrador: {{$campaign->administrator->name['first'].
                         ' '. $campaign->administrator->name['last']}}</li>
-                                <hr>
-                                <li data-icon="keyboard_arrow_right">
-                                    Inicio: {{date('Y-m-d',$campaign->filters['date']['start']->sec)}}</li>
-                                <hr>
-                                <li data-icon="keyboard_arrow_right">
-                                    Terminación: {{date('Y-m-d',$campaign->filters['date']['end']->sec)}}</li>
-                                <hr>
-                                <li data-icon="keyboard_arrow_right">Dias:
-                                    @if(isset($campaign->filters['week_days'] ))
-                                        @foreach($campaign->filters['week_days'] as $dia)
-                                            {{ trans('days.'.$dia) }},
-                                        @endforeach
-                                    @else
-                                        no definido
-                                    @endif
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="card-action ">
-                            <a  class="blue-text" href="{{route('campaigns::show', ['id' => $campaign->id])}}">Ver más detalles</a>
+                                    <hr>
+                                    <li data-icon="keyboard_arrow_right">
+                                        Inicio: {{date('Y-m-d',$campaign->filters['date']['start']->sec)}}</li>
+                                    <hr>
+                                    <li data-icon="keyboard_arrow_right">
+                                        Terminación: {{date('Y-m-d',$campaign->filters['date']['end']->sec)}}</li>
+                                    <hr>
+                                    <li data-icon="keyboard_arrow_right">Dias:
+                                        @if(isset($campaign->filters['week_days'] ))
+                                            @foreach($campaign->filters['week_days'] as $dia)
+                                                {{ trans('days.'.$dia) }},
+                                            @endforeach
+                                        @else
+                                            no definido
+                                        @endif
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="card-action ">
+                                <a  class="blue-text" href="{{route('campaigns::show', ['id' => $campaign->id])}}">Ver más detalles</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
 
@@ -134,14 +136,11 @@
 
 @section('scripts')
     {!! HTML::script('js/jquery-validation/dist/jquery.validate.js') !!}
-
     <script>
         $(document).ready(function(){
             // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
             $('.modal-trigger').leanModal();
         });
-
-
         $("#validate").validate({
             rules: {
                 campaign: {
