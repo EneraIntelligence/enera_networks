eb84a8391146815e3286d07d879f354087158bc6
Pedro Luna <pluna@enera.mx>
2016-06-20 17:07:46 -0500

    [NEW] DashboardController > index

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -45,7 +45,7 @@ class DashboardController extends Controller
                 'accumulated' => $summary_network->accumulated['users']['total'],
                 'male' => array_sum($summary_network->accumulated['users']['demographic']['male']),
                 'female' => array_sum($summary_network->accumulated['users']['demographic']['female']),
-                'diff' => ($t3->accumulated['users']['total'] - $t2->accumulated['users']['total']) != 0 ?
+                'diff' => ($t2->accumulated['users']['total'] - $t3->accumulated['users']['total']) != 0 ?
                     100 * ((($summary_network->accumulated['users']['total'] - $t2->accumulated['users']['total'])
                             - ($t2->accumulated['users']['total'] - $t3->accumulated['users']['total']))
                         / ($t2->accumulated['users']['total'] - $t3->accumulated['users']['total'])) : 0,
