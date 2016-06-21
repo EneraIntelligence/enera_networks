f750caa2fe2071598490f7de824bce886171dbbc
Eder Díaz <ederchrono@gmail.com>
2016-06-21 11:25:41 -0500

    rediseño de lista de campañas

--- resources/views/campaign/index.blade.php
+++ resources/views/campaign/index.blade.php
@@ -7,128 +7,57 @@
 @stop
 
 @section('content')
+
+
     @if( count($campaigns) > 0 )
-        <ul class="collapsible hide-on-med-and-up menu grey darken-3 white-text" data-collapsible="accordion">
 
 
-        <li>
-            <div class="collapsible-header grey darken-3 white-text">
-                <div class="row zero">
-                    <div class="col s6" style="text-align: center;">
-                        <span>Nombre</span>
-                    </div>
-                </div>
-            </div>
-        </li>
-
-
-        @foreach($campaigns as $campaign)
-            <li>
-                <div class="collapsible-header grey darken-3 white-text">
-                    <span class="icon-menu">
-                        {{strtoupper(substr($campaign->name, 0, 2)) }}
-                    </span>
-                    <span>
-                        {{$campaign->name}}
-                    </span>
-                </div>
-                <div class="collapsible-body grey">
-                    <div class="container">
-                        <ul class="list grey white-text card_info" >
-                            <li data-icon="keyboard_arrow_right">Status: <span class="light-text">{{$campaign->status}}</span></li>
-                            <li data-icon="keyboard_arrow_right">Administrador: <span class="light-text">{{$campaign->administrator->name['first'].
-                        ' '. $campaign->administrator->name['last']}}</span></li>
-                            <li data-icon="keyboard_arrow_right">
-                                Inicio: <span class="light-text">{{date('Y-m-d',$campaign->filters['date']['start']->sec)}}</span></li>
-                            <li data-icon="keyboard_arrow_right">
-                                Fin: <span class="light-text">{{date('Y-m-d',$campaign->filters['date']['end']->sec)}}</span></li>
-                            <li data-icon="keyboard_arrow_right">Dias:
-                                @if(isset($campaign->filters['week_days'] ))
-                                    @foreach($campaign->filters['week_days'] as $dia)
-                                        <span class="light-text">{{ trans('days.'.$dia) }},</span>
-                                    @endforeach
-                                @else
-                                    <span class="light-text">no definido</span>
-                                @endif
-                            </li>
-                        </ul>
-                        <a href="{{route('campaigns::show', ['id' => $campaign->id])}}" class="waves-effect waves-light btn btn-mobil">Ver más detalles</a>
-                    </div>
-                </div>
-            </li>
-        @endforeach
-
-
-    </ul>
-
-    @endif
-
-
-    @if( count($campaigns) == 0 )
-        <div class="container hide-on-med-and-up">
-            <h5 style="text-align:center;">Aún no tienes campañas. <br>Crea tu primer campaña.</h5>
-        </div>
-    @endif
-
-    <div class="hide-on-small-only">
         <div class="container">
             <h3>Campañas</h3>
+
             <div class="row">
+
                 @foreach($campaigns as $campaign)
-                    <div class="col m4 l3">
-                        <div class="card grey darken-3 white-text card-campaign" style="min-height: 610px;">
-                            <div class="card-content white-text">
-                                <div class="card-image margin-image">
-                                    <img src="{{"https://s3-us-west-1.amazonaws.com/enera-publishers/items/". ($campaign->interaction['name'] != 'survey' ? $campaign->content['images']['large'] : $campaign->content['images']['survey'])}}"
-                                         class="responsive-img">
-                                    {{--<span class="card-title">{{$campaign->name}}</span>--}}
-                                </div>
-                                {{--<span class="card-title black-text">{{$campaign->name}}</span>--}}
-                                <ul class="list white-text">
-                                    <li data-icon="keyboard_arrow_right">Nombre: <span class="light-text">{{$campaign->name}}</span></li>
-                                    <hr>
-                                    <li data-icon="keyboard_arrow_right">Administrador: <span class="light-text">{{$campaign->administrator->name['first'].
-                        ' '. $campaign->administrator->name['last']}}</span></li>
-                                    <hr>
-                                    <li data-icon="keyboard_arrow_right">
-                                        Inicio: <span class="light-text">
-                                            {{date('y-m-d',$campaign->filters['date']['start']->sec)}}
-                                        </span></li>
-                                    <hr>
-                                    <li data-icon="keyboard_arrow_right">
-                                        Fin: <span class="light-text">{{date('y-m-d',$campaign->filters['date']['end']->sec)}}</span></li>
-                                    <hr>
-                                    <li data-icon="keyboard_arrow_right">Dias:
-                                        @if(isset($campaign->filters['week_days'] ))
-                                            @foreach($campaign->filters['week_days'] as $dia)
-                                                <span class="light-text">{{ trans('days.'.$dia) }}</span>,
-                                            @endforeach
-                                        @else
-                                            <span class="light-text">no definido</span>
-                                        @endif
-                                    </li>
-                                </ul>
+
+
+                <div class="col s12 m6 l4">
+
+                    <div class="card">
+
+                        <div class="row">
+
+                            <div class="thumb left">
+                                <img src="{{"https://s3-us-west-1.amazonaws.com/enera-publishers/items/". ($campaign->interaction['name'] != 'survey' ? $campaign->content['images']['large'] : $campaign->content['images']['survey'])}}">
                             </div>
-                            <div class="card-action ">
-                                <a  class="blue-text" href="{{route('campaigns::show', ['id' => $campaign->id])}}">Ver más detalles</a>
-                            </div>
+
+
+                            <strong>{{$campaign->name}} <br> </strong>
+                            <span>
+                                Vistos: 100 <br>
+                                Interacciones:12
+                            </span>
+                            <div class="divider" style="margin-top:7px;"></div>
+                            <span class="grey-text bottom-left" style="margin-left:77px;">{{$campaign->interaction['name']}}</span>
+                            <a class="bottom-right" style="margin-right:10px;" href="{{route('campaigns::show', ['id' => $campaign->id])}}">
+                                Detalles
+                            </a>
+
                         </div>
-                    </div>
+
+
+
+                    </div> <!-- end card -->
+
+
+                </div> <!-- end col s12 -->
+
                 @endforeach
 
+            </div><!-- end row -->
 
-                @if( count($campaigns) == 0 )
-                    <div class="empty-message" style="width: 80%; margin:20px auto 0;">
-                        <img style="margin:0 auto; width:200px; display:block;" src="{!! URL::asset('images/icons/banner_new.svg') !!}" alt="">
-                        <h3 style="text-align:center;">
-                            Aún no tienes campañas. <br>Crea tu primer campaña.
-                        </h3>
-                    </div>
-                @endif
-
-            </div>
         </div>
-    </div>
+
+    @endif;
 
     <div class="fixed-action-btn" style="bottom: 55px; right: 24px;">
 
