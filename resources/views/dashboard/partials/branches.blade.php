8370416df2ef36f9a89291660b6a0b9076c6bb82
Alfonso Rodriguez <darkdreke@gmail.com>
2016-07-12 16:39:22 -0500

    added the rules icon-left and dark-green to dashboard.scss added the icon to span in card branches

--- resources/views/dashboard/partials/branches.blade.php
+++ resources/views/dashboard/partials/branches.blade.php
@@ -2,21 +2,70 @@
     <img class="responsive-img" src="{{asset('images/dashboard/imagen_nodos.jpg')}}" alt="">
 </div>
 
-<div class="section" style="font-size: 25px; padding-top:0;">
+<div class="section" style="font-size: 25px; padding:0;">
     <span style="font-weight: 400;">Nodos</span>
 </div>
 
-<div class="section">
-    <div class="card black-text" style="padding: 5px 0 10px 0;">
-        <div style="margin: 0 auto; width: 50%;">
-            <span style="font-weight: 300;font-size: 17px;">nodo name</span>
-            <span class="peity_accessed peity_data" style="margin: 5px 0;">
-                1,2,3,4,5,6,1,2,3,4,1,2,3,4,5,6,1,2,3</span>
-            <?php
+<div class="section" style="padding: 0;">
+    <div class="card black-text" style="margin-bottom: 5px;">
+        <div class="container" style="width: 90%;">
+            <p style="padding: 5px 0 0 0; margin-bottom: 5px;">Plaza sendero</p>
+            <div class="divider grey darken-1"></div>
+            <div class="row">
+                <div class="col s6 right-align">
+                    <span style="font-weight: 300;"><i class="material-icons icon-left">&#xE62B;</i>Connexíones:</span>
+                </div>
+                <div class="col s6 right-align">
+                    <span style="font-weight: 200;font-size:19px;">150,000</span>
+                </div>
+                <div class="col s12 right-align">
+                        <span class="dark-green" style="font-size:9px; font-weight: 300; vertical-align:top;">{{date('d-M',strtotime( "-7 days" ))}}
+                            - {{date('d-M')}}</span>
+                </div>
 
-            ?>
-            <span style="font-weight: 300;font-size: 10px; padding-bottom: 15px;">Dias restantes: {{0}}</span>
+            </div>
         </div>
-        <a class="waves-effect waves-teal btn-fla" id="button-right" href=""><i class="material-icons right" style="margin-left: 0;">keyboard_arrow_right</i>Ver</a>
     </div>
+    <div class="card black-text" style="margin-bottom: 2px;">
+        <div class="container" style="width: 90%;">
+            <p style="padding: 5px 0 0 0; margin-bottom: 5px;">Plaza sendero</p>
+            <div class="divider grey darken-1"></div>
+            <div class="row">
+                <div class="col s6 right-align">
+                    <span style="font-weight: 300;"><i class="material-icons icon-left">&#xE62B;</i>Connexíones:</span>
+                </div>
+                <div class="col s6 right-align">
+                    <span style="font-weight: 200;font-size:19px;">150,000</span>
+                </div>
+                <div class="col s12 right-align">
+                        <span class="dark-green" style="font-size:9px; font-weight: 300; vertical-align:top;">{{date('d-M',strtotime( "-7 days" ))}}
+                            - {{date('d-M')}}</span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="card black-text" style="margin-bottom: 5px;">
+        <div class="container" style="width: 90%;">
+            <p style="padding: 5px 0 0 0; margin-bottom: 5px;">Plaza sendero</p>
+            <div class="divider grey darken-1"></div>
+            <div class="row">
+                <div class="col s6 right-align">
+                    <span style="font-weight: 300;"><i class="material-icons icon-left">&#xE62B;</i>Connexíones:</span>
+                </div>
+                <div class="col s6 right-align">
+                    <span style="font-weight: 200;font-size:19px;">150,000</span>
+                </div>
+                <div class="col s12 right-align">
+                        <span class="dark-green" style="font-size:9px; font-weight: 300; vertical-align:top;">{{date('d-M',strtotime( "-7 days" ))}}
+                            - {{date('d-M')}}</span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="section card-bottom">
+    <a style="width:80%; margin-left:10%;"
+       class="btn cyan" href="#!">Ver más</a>
 </div>
