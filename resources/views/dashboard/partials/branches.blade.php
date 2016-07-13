c00622fd24997660bbce9b3ca9a35ba65577e177
Alfonso Rodriguez <darkdreke@gmail.com>
2016-07-13 09:48:28 -0500

    fix the col in branch´s card

--- resources/views/dashboard/partials/branches.blade.php
+++ resources/views/dashboard/partials/branches.blade.php
@@ -12,32 +12,13 @@
             <p style="padding: 5px 0 0 0; margin-bottom: 5px;">Plaza sendero</p>
             <div class="divider grey darken-1"></div>
             <div class="row">
-                <div class="col s6 right-align">
+                <div class="col s7 m6 right-align">
                     <span style="font-weight: 300;"><i class="material-icons icon-left">&#xE62B;</i>Connexíones:</span>
                 </div>
-                <div class="col s6 right-align">
+                <div class="col s5 m6 right-align">
                     <span style="font-weight: 200;font-size:19px;">150,000</span>
                 </div>
-                <div class="col s12 right-align">
-                        <span class="dark-green" style="font-size:9px; font-weight: 300; vertical-align:top;">{{date('d-M',strtotime( "-7 days" ))}}
-                            - {{date('d-M')}}</span>
-                </div>
-
-            </div>
-        </div>
-    </div>
-    <div class="card black-text" style="margin-bottom: 2px;">
-        <div class="container" style="width: 90%;">
-            <p style="padding: 5px 0 0 0; margin-bottom: 5px;">Plaza sendero</p>
-            <div class="divider grey darken-1"></div>
-            <div class="row">
-                <div class="col s6 right-align">
-                    <span style="font-weight: 300;"><i class="material-icons icon-left">&#xE62B;</i>Connexíones:</span>
-                </div>
-                <div class="col s6 right-align">
-                    <span style="font-weight: 200;font-size:19px;">150,000</span>
-                </div>
-                <div class="col s12 right-align">
+                <div class="col s12 m 12 right-align">
                         <span class="dark-green" style="font-size:9px; font-weight: 300; vertical-align:top;">{{date('d-M',strtotime( "-7 days" ))}}
                             - {{date('d-M')}}</span>
                 </div>
@@ -50,13 +31,32 @@
             <p style="padding: 5px 0 0 0; margin-bottom: 5px;">Plaza sendero</p>
             <div class="divider grey darken-1"></div>
             <div class="row">
-                <div class="col s6 right-align">
+                <div class="col s7 m6 right-align">
                     <span style="font-weight: 300;"><i class="material-icons icon-left">&#xE62B;</i>Connexíones:</span>
                 </div>
-                <div class="col s6 right-align">
+                <div class="col s5 m6 right-align">
                     <span style="font-weight: 200;font-size:19px;">150,000</span>
                 </div>
-                <div class="col s12 right-align">
+                <div class="col s12 m 12 right-align">
+                        <span class="dark-green" style="font-size:9px; font-weight: 300; vertical-align:top;">{{date('d-M',strtotime( "-7 days" ))}}
+                            - {{date('d-M')}}</span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="card black-text" style="margin-bottom: 5px;">
+        <div class="container" style="width: 90%;">
+            <p style="padding: 5px 0 0 0; margin-bottom: 5px;">Plaza sendero</p>
+            <div class="divider grey darken-1"></div>
+            <div class="row">
+                <div class="col s7 m6 right-align">
+                    <span style="font-weight: 300;"><i class="material-icons icon-left">&#xE62B;</i>Connexíones:</span>
+                </div>
+                <div class="col s5 m6 right-align">
+                    <span style="font-weight: 200;font-size:19px;">150,000</span>
+                </div>
+                <div class="col s12 m 12 right-align">
                         <span class="dark-green" style="font-size:9px; font-weight: 300; vertical-align:top;">{{date('d-M',strtotime( "-7 days" ))}}
                             - {{date('d-M')}}</span>
                 </div>
