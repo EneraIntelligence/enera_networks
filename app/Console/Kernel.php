f4d08620779a896c09d8e296182f194bd4bca51b
Eder Díaz <ederchrono@gmail.com>
2016-10-05 15:56:52 -0500

    automatizing command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('inspire')
                  ->hourly();
+        $schedule->command('enera:RecurrentDevices')->daily();
 
     }
 }
