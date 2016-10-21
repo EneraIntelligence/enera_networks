41f7b84360556c6af0c3c5b8601f613349f6296b
Alfonso Rodriguez <darkdreke@gmail.com>
2016-10-21 15:10:21 -0500

    Added of smartlook for recording

--- resources/views/layouts/mainmat.blade.php
+++ resources/views/layouts/mainmat.blade.php
@@ -23,6 +23,14 @@
 
 <!-- App - CSS -->
     {!! HTML::style('assets/css/app.css') !!}
+    <script type="text/javascript">
+        window.smartlook||(function(d) {
+            var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+            var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+            c.charset='utf-8';c.src='//rec.smartlook.com/recorder.js';h.appendChild(c);
+        })(document);
+        smartlook('init', 'ab997449c1233defbf315e0e0123d0e5a5fce206');
+    </script>
     <style>
         .zero a {
             font-size: 15px;
